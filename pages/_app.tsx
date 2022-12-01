87cdf02a398b7a5f4ef6e245a7c41ec1c4d32461
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-01 16:51:40 -0300

    I remove unused import of AlchemyProvider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
