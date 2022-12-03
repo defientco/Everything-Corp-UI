9d895263c3d53604651a9bee95fec4fb1df527d8
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-03 18:23:24 -0300

    I init <MintButton />

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -1,6 +1,9 @@
+import MintButton from "../MintButton"
+
 const MintPage = () => {
-   return <div className="flex justify-center align-center text-4xl pt-10">
-    Mint Page
+   return <div className="flex flex-col items-center justify-between text-4xl pt-10">
+        <div>Mint Page</div>
+        <MintButton />
     </div>
 }
 
