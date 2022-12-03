c9114341a67d5d563cd7a98f05f2a9250809735e
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-03 20:41:49 -0300

    I call purchase on button click.

--- components/MintButton/MintButton.tsx
+++ components/MintButton/MintButton.tsx
@@ -1,5 +1,15 @@
+import { useSigner } from "wagmi"
+import purchase from "../../lib/purchase"
+
 const MintButton = () => {
-    return <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Mint</button>
+    const {data: signer} = useSigner()
+
+    const handleClick = async() => {
+        if (!signer) return
+        await purchase(signer)
+    }
+
+    return <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Mint</button>
 }
 
 export default MintButton
