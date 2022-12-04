0f65748a1905f91dcbdfbb544958cc0b85286622
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-04 09:40:51 -0300

    I remove unused callback params.

--- lib/purchase.tsx
+++ lib/purchase.tsx
@@ -2,18 +2,15 @@ import { ethers, Signer } from "ethers"
 import abi from "./abi-cre8ors.json"
 import handleTxError from "./handleTxError"
 
-const purchase = async (signer: Signer, onPending?: any, onConfirmed?: any) => {
+const purchase = async (signer: Signer) => {
     const contract = new ethers.Contract(String(process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS), abi, signer)
-    console.log("CONTRACT", contract)
     try {
         const tx = await contract.purchase(1)
-        onPending?.(tx)
         const receipt = await tx.wait()
-        onConfirmed?.(receipt)
         return receipt
     } catch (err) {
-        console.error(err)
         handleTxError(err)
+        return err
     }
 }
 
