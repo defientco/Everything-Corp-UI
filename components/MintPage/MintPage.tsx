af04512b1aedf04ba7c0b925d8959e0b4cabcb4c
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-04 12:13:41 -0300

    I center Mint Page components.

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -1,7 +1,7 @@
 import MintButton from "../MintButton"
 
 const MintPage = () => {
-   return <div className="flex flex-col items-center justify-between text-4xl pt-10">
+   return <div className="flex flex-col items-center justify-around text-4xl pt-10 h-[75vh]">
         <div>Mint Page</div>
         <MintButton />
     </div>
