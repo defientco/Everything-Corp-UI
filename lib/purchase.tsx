f25e6f24a2d207395882bd61df4745312efebf81
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-04 09:40:10 -0300

    I add functioning purchase util.

--- lib/purchase.tsx
+++ lib/purchase.tsx
@@ -1,14 +1,19 @@
 import { ethers, Signer } from "ethers"
 import abi from "./abi-cre8ors.json"
+import handleTxError from "./handleTxError"
 
-const purchase = (signer: Signer) => {
+const purchase = async (signer: Signer, onPending?: any, onConfirmed?: any) => {
     const contract = new ethers.Contract(String(process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS), abi, signer)
     console.log("CONTRACT", contract)
-    const tx = contract.purchase(1)
     try {
-        const tx = contract.purchase(1)
+        const tx = await contract.purchase(1)
+        onPending?.(tx)
+        const receipt = await tx.wait()
+        onConfirmed?.(receipt)
+        return receipt
     } catch (err) {
         console.error(err)
+        handleTxError(err)
     }
 }
 
