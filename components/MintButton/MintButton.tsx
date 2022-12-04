7cad9cdb1105a519d4987d02a532978f825defbd
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-04 09:41:15 -0300

    I add disabled state to MintButton when tx is in-progress.

--- components/MintButton/MintButton.tsx
+++ components/MintButton/MintButton.tsx
@@ -1,15 +1,21 @@
+import Image from "next/image"
+import { useState } from "react"
 import { useSigner } from "wagmi"
 import purchase from "../../lib/purchase"
 
 const MintButton = () => {
+    const [loading, setLoading] = useState(false)
     const {data: signer} = useSigner()
 
     const handleClick = async() => {
         if (!signer) return
+        setLoading(true)
         await purchase(signer)
+        setLoading(false)
     }
 
-    return <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Mint</button>
+    const className = `${loading ? "bg-blue-500/50" : "bg-blue-500"} ${!loading && "hover:bg-blue-700"} text-white font-bold py-2 px-4 rounded`
+    return <button onClick={handleClick} disabled={loading} className={className}>{loading ? <Image src="/spinner.gif" alt="spinner" width={50} height={50} /> : "Mint"}</button>
 }
 
 export default MintButton
