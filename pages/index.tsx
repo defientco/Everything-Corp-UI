3b78a2fb6dbfaa9b9ed845b8b9378d25c554051d
sameer rasa <crazzywizard@gmail.com>
2022-12-07 10:25:41 -0500

    refactored mint button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,38 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import SeoHead from '../components/SeoHead';
+import TextArea from '../components/TextArea';
 import styles from '../styles/Home.module.css';
-
+import abi from '../lib/abi-allow-list.json';
+import MintButton from '../components/MintButton';
+import purchase from '../lib/purchase';
+const LABEL = 'Why do you want to join Crea8tors?';
 const Home: NextPage = () => {
+  const [value, setValue] = useState('');
+  const contractAddress = String(process.env.NEXT_PUBLIC_ALLOW_LIST_ADDRESS);
   return (
     <div className={styles.container}>
       <SeoHead title="CRE8ORS" description="CRE8ORS coming soon" image="" />
 
       <main className={styles.main}>
-
-        <h1 className={styles.title}>
-            CRE8ORS
-        </h1>
+        <h1 className={styles.title}>CRE8ORS</h1>
 
         <div className="flex flex-col gap-4">
-
-            
-            <a href="#" className="py-10 px-3 border rounded-lg border-black w-[500px]">
-              <div className="text-2xl">Minting</div>
-              <p>Coming soon</p>
-              <ConnectButton />
-            </a>
+          <a href="#" className="py-10  border rounded-lg border-black w-[500px]">
+            <div className="justify-content align-items-center">
+              <TextArea value={value} label={LABEL} setValue={setValue} />
+              {value.length > 0 && (
+                <MintButton
+                  contractAddress={contractAddress}
+                  abi={abi}
+                  purchase={purchase}
+                  formResponse={value}
+                />
+              )}
+            </div>
+          </a>
         </div>
       </main>
     </div>
