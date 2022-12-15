592799ff6b6b773e78a12af18b18cc60012f1528
sameer rasa <crazzywizard@gmail.com>
2022-12-15 16:43:20 -0500

    trying to figure out serverless issues

--- pages/api/allowlist/index.ts
+++ pages/api/allowlist/index.ts
@@ -1,29 +1,30 @@
 /* eslint-disable class-methods-use-this */
-import { createHandler, Post, Body, Get } from "next-api-decorators"
+import { createHandler, Post, Body, ValidationPipe } from "next-api-decorators"
 import { ApplicantDTO } from "../../../DTO/applicant.dto"
 import { db } from "../../../utils/db"
+import { AllowListAuthGuard } from "../../../middleware/auth.middleware"
+import { ApplicantRegistered } from "../../../middleware/unique.middleware"
 
 class AllowListApplicants {
   @Post()
-  async addAllowListApplicant(@Body() body: ApplicantDTO) {
-    console.log(body)
+  @AllowListAuthGuard()
+  @ApplicantRegistered()
+  async addAllowListApplicant(@Body(ValidationPipe) body: ApplicantDTO) {
     const { twitterHandle, walletAddress, reason, creatorType } = body
-    console.log(twitterHandle, walletAddress, reason, creatorType)
     const collection = db.collection("allowListApplicants")
-    console.log("got database")
-    const result = await collection.doc().set({
-      twitterHandle,
-      walletAddress,
-      reason,
-      creatorType,
-    })
-    console.log("added to database")
-    return result
-  }
-
-  @Get()
-  async helloWorld() {
-    return "Hello World"
+    try {
+      console.log("Adding applicant to allow list")
+      const result = await collection.doc().set({
+        twitterHandle,
+        walletAddress,
+        reason,
+        creatorType,
+      })
+      console.log("Applicant added to allow list")
+      return result
+    } catch (error) {
+      throw new Error(error)
+    }
   }
 }
 
