c53e96a056103da57cb6635031e88b44cb56bfc6
sameer rasa <crazzywizard@gmail.com>
2022-12-15 17:23:09 -0500

    back to the basics

--- pages/api/allowlist/index.ts
+++ pages/api/allowlist/index.ts
@@ -1,26 +1,15 @@
-/* eslint-disable class-methods-use-this */
-import { createHandler, Post, Body } from "next-api-decorators"
-import { ApplicantDTO } from "../../../DTO/applicant.dto"
+import { NextApiResponse, NextApiRequest } from "next"
+
 import { db } from "../../../utils/db"
 
-class AllowListApplicants {
-  @Post()
-  async addAllowListApplicant(@Body() body: ApplicantDTO) {
-    const { twitterHandle, walletAddress, reason, creatorType } = body
-    const collection = db.collection("allowListApplicants")
-    try {
-      const result = await collection.add({
-        twitterHandle,
-        walletAddress,
-        reason,
-        creatorType,
-      })
-      console.log("Applicant added to allow list")
-      return result
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { twitterHandle, walletAddress, reason, creatorType } = req.body
+  const collection = db.collection("allowListApplicants")
+  const result = await collection.add({
+    twitterHandle,
+    walletAddress,
+    reason,
+    creatorType,
+  })
+  res.status(200).json({ result })
 }
-
-export default createHandler(AllowListApplicants)
