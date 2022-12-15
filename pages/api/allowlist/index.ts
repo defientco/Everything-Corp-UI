6eba7854e0a9cae7d3c6514477a7074a62a735ee
sameer rasa <crazzywizard@gmail.com>
2022-12-15 13:14:35 -0500

    create allowlist applicant in api form

---
+++ pages/api/allowlist/index.ts
@@ -0,0 +1,25 @@
+/* eslint-disable class-methods-use-this */
+import { createHandler, Post, Body, ValidationPipe } from "next-api-decorators"
+import { ApplicantDTO } from "../../../DTO/applicant.dto"
+import { db } from "../../../utils/db"
+import { AllowListAuthGuard } from "../../../middleware/auth.middleware"
+import { ApplicantRegistered } from "../../../middleware/unique.middleware"
+
+class AllowListApplicants {
+  @Post()
+  @AllowListAuthGuard()
+  @ApplicantRegistered()
+  async addAllowListApplicant(@Body(ValidationPipe) body: ApplicantDTO) {
+    const { twitterHandle, walletAddress, reason, creatorType } = body
+    const collection = db.collection("allowListApplicants")
+    const result = await collection.doc().set({
+      twitterHandle,
+      walletAddress,
+      reason,
+      creatorType,
+    })
+    return result
+  }
+}
+
+export default createHandler(AllowListApplicants)
