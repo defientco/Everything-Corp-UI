a9c838032ea6c188aecd0a433b6ecb0cf518d8b1
sameer rasa <crazzywizard@gmail.com>
2022-12-15 15:42:37 -0500

    trying to fix issues

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase-admin/firestore"
 
 const params: ServiceAccount = {
   projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
-  privateKey: JSON.parse(process.env.SERVICE_ACCOUNT_PRIVATE_KEY),
+  privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
   clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
 }
 const apps = getApps()
