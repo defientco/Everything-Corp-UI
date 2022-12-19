5efbbd9c0c62e7327b8945ce1399e9a264e4c9d1
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-19 16:58:57 -0500

    WIP

--- components/Cre8orTile/Cre8orTile.tsx
+++ components/Cre8orTile/Cre8orTile.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image"
+import { useContractRead } from "wagmi"
 import getIpfsLink from "../../utils/getIpfsLink"
+import abi from "../../lib/abi-cre8ors.json"
 
 const Cre8orTile = (props: any) => {
     const { nft } = props
     const tokenId = parseInt(nft.id.tokenId, 16)
+    const { data } = useContractRead({
+        address: process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS,
+        abi,
+        functionName: 'cre8ingPeriod',
+    })
+
+    console.log("data", data)
 
     return (
         <div className="flex flex-col items-center">
