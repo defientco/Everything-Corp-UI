ff03bc9533cd4e5d2341045a2dcf28744c1c3bc4
sameer rasa <crazzywizard@gmail.com>
2022-12-19 16:00:30 -0500

    disable clicking if sending

--- components/AllowListForm/AllowListForm.tsx
+++ components/AllowListForm/AllowListForm.tsx
@@ -95,7 +95,7 @@ const AllowListForm: FC<AllowListFormProps> = ({
             className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline disabled:opacity-25 disabled:cursor-not-allowed"
             type="button"
             onClick={handleSignUp}
-            disabled={disabled}
+            disabled={disabled || loading}
           >
             Sign Up
           </button>
