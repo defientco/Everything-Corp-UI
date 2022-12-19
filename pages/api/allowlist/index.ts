87d714cfbf17708f50270380fec4016e651670f7
sameer rasa <crazzywizard@gmail.com>
2022-12-19 14:07:29 -0500

    reverting

--- pages/api/allowlist/index.ts
+++ pages/api/allowlist/index.ts
@@ -2,18 +2,18 @@
 import { createHandler, Post, Body, ValidationPipe } from "next-api-decorators"
 import { ApplicantDTO } from "../../../DTO/applicant.dto"
 import dbConnect from "../../../utils/db"
+import { ApplicantRegistered } from "../../../middleware/unique.middleware"
 import AllowList from "../../../Models/AllowList"
+import { AllowListAuthGuard } from "../../../middleware/auth.middleware"
 
 class AllowListApplicants {
   @Post()
+  @AllowListAuthGuard()
+  @ApplicantRegistered()
   async addAllowListApplicant(@Body(ValidationPipe) body: ApplicantDTO) {
     try {
-      console.log("connecting to db")
       await dbConnect()
-      console.log("connected to db")
-      console.log("writing to db")
       const result = await AllowList.create(body)
-      console.log("wrote to db")
       return { sucess: true, result }
     } catch (error) {
       throw new Error(error)
