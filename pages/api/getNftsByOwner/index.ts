b54792532184feb6301c74b3c4dd18ce6b048f8b
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-19 15:38:09 -0500

    I update getNftsByOwner to work with FE.

--- pages/api/getNftsByOwner/index.ts
+++ pages/api/getNftsByOwner/index.ts
@@ -6,7 +6,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log("ownerAddress", ownerAddress)
   console.log("process.env.ALCHEMY_API_KEY", process.env.ALCHEMY_API_KEY)
   try {
-    const response = await axios.get(`https://eth-goerli.g.alchemy.com/nft/v2/${process.env.ALCHEMY_API_KEY}/getNFTs?owner=${ownerAddress}&contractAddresses%5B%5D=${NEXT_PUBLIC_CRE8ORS_ADDRESS}`)
+    const response = await axios.get(`https://eth-goerli.g.alchemy.com/nft/v2/${process.env.ALCHEMY_API_KEY}/getNFTs?owner=${ownerAddress}&contractAddresses%5B%5D=${process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS}`)
+    console.log("data", response.data)
     res.status(200).json(response.data)
   } catch (err) {
     res.status(500).json(err)
