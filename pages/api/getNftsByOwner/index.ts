9efac50c96aaa7198ef0ff8b6682b569851de83a
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-19 15:02:48 -0500

    I init API endpoint to return NFTs for a given owner.

---
+++ pages/api/getNftsByOwner/index.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+import { NextApiRequest, NextApiResponse } from "next"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { ownerAddress } = req.query
+  console.log("ownerAddress", ownerAddress)
+  console.log("process.env.ALCHEMY_API_KEY", process.env.ALCHEMY_API_KEY)
+  try {
+    const response = await axios.get(`https://eth-goerli.g.alchemy.com/nft/v2/${process.env.ALCHEMY_API_KEY}/getNFTs?owner=${ownerAddress}&contractAddresses%5B%5D=${NEXT_PUBLIC_CRE8ORS_ADDRESS}`)
+    res.status(200).json(response.data)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  
+}
+export default handler
