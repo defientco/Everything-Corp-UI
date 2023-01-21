fc50ce44a9298027e171974859a6a4dcf3f7238b
Sweets Sweetman <sweetmantech@gmail.com>
2023-01-21 10:50:36 -0300

    I init <ClaimPage /> component.

---
+++ components/ClaimPage/ClaimPage.tsx
@@ -0,0 +1,14 @@
+import MintButton from "../MintButton"
+import abi from "../../lib/abi-cre8ors.json"
+
+function ClaimPage() {
+  const contractAddress = String(process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS)
+  return (
+    <div className="flex flex-col items-center justify-around text-4xl pt-10 h-[75vh]">
+      <div>Claim Page</div>
+      <MintButton contractAddress={contractAddress} abi={abi} />
+    </div>
+  )
+}
+
+export default ClaimPage
