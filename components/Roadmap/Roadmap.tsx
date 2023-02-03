4b0e8bc3d9d3126d688aa601b121bbcebc03c952
Sweets Sweetman <sweetmantech@gmail.com>
2023-02-03 10:21:43 -0300

    I switch cards from <button /> => <div />

--- components/Roadmap/Roadmap.tsx
+++ components/Roadmap/Roadmap.tsx
@@ -5,7 +5,7 @@ const Roadmap = (props: any) => {
   const { setActiveStep } = props
   const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
   const className =
-    "border border-sky-500 h-[80vh] min-w-[100vw] sm:min-w-[30vw] rounded-2xl p-5 overflow-x-scroll"
+    "border border-sky-500 h-[80vh] min-w-[100vw] sm:min-w-[30vw] rounded-2xl p-5 overflow-x-scroll snap-always snap-center"
   const RoadmapCards: String[] = [
     "Watch Trailer",
     "Mysteries Revealed",
@@ -31,11 +31,16 @@ const Roadmap = (props: any) => {
   }, [scrollPosition])
 
   return (
-    <div className="sm:h-screen h-[200vh] w-[200vw] flex items-center  gap-3 pl-[154vw] sm:pl-[50vw] overflow-x-scroll">
+    <div className="sm:h-screen h-[200vh] w-[200vw] snap-x snap-mandatory flex nowrap items-center  gap-3 pl-[154vw] sm:pl-[50vw] overflow-x-auto">
       {RoadmapCards.map((item) => (
-        <button key={item as any} className={className} onClick={() => setActiveStep(4)}>
+        <div
+          key={item as any}
+          className={className}
+          onClick={() => setActiveStep(4)}
+          aria-hidden="true"
+        >
           {item}
-        </button>
+        </div>
       ))}
     </div>
   )
