7180401715fb4369aaa6b355ae676e94fa3cf09d
Sweets Sweetman <sweetmantech@gmail.com>
2023-02-03 10:02:26 -0300

    I DRY roadmap code.

--- components/Roadmap/Roadmap.tsx
+++ components/Roadmap/Roadmap.tsx
@@ -6,6 +6,15 @@ const Roadmap = (props: any) => {
   const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
   const className =
     "border border-sky-500 h-[80vh] min-w-[100vw] sm:min-w-[30vw] rounded-2xl p-5 overflow-x-scroll"
+  const RoadmapCards: String[] = [
+    "Watch Trailer",
+    "Mysteries Revealed",
+    "Storyline",
+    "Play Whitelist Quiz",
+    "COMING SOON",
+    "COMING SOON",
+    "COMING SOON",
+  ]
 
   useEffect(() => {
     const handleScroll = (event) => {
@@ -23,27 +32,11 @@ const Roadmap = (props: any) => {
 
   return (
     <div className="sm:h-screen h-[200vh] w-[200vw] flex items-center  gap-3 pl-[154vw] sm:pl-[50vw] overflow-x-scroll">
-      <button className={className} onClick={() => setActiveStep(4)}>
-        Watch Trailer
-      </button>
-      <button className={className} onClick={() => setActiveStep(4)}>
-        Mysteries Revealed
-      </button>
-      <button className={className} onClick={() => setActiveStep(4)}>
-        Storyline
-      </button>
-      <button className={className} onClick={() => setActiveStep(4)}>
-        Play Whitelist Quiz
-      </button>
-      <button disabled className={className} onClick={() => setActiveStep(4)}>
-        COMING SOON
-      </button>
-      <button disabled className={className} onClick={() => setActiveStep(4)}>
-        COMING SOON
-      </button>
-      <button disabled className={className} onClick={() => setActiveStep(4)}>
-        COMING SOON
-      </button>
+      {RoadmapCards.map((item) => (
+        <button key={item as any} className={className} onClick={() => setActiveStep(4)}>
+          {item}
+        </button>
+      ))}
     </div>
   )
 }
