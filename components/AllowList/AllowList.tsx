11fea42189934784abaf09e46301bb06b1350761
sameer rasa <crazzywizard@gmail.com>
2023-02-04 19:07:05 -0500

    refactored AllowList component

--- components/AllowList/AllowList.tsx
+++ components/AllowList/AllowList.tsx
@@ -7,28 +7,28 @@ import useWindowSize from "../../lib/useWindowSize"
 import { useCre8orsProvider } from "../../providers/Crea8orsProvider"
 import SkeletonCard from "../SkeletonCard"
 import ChoiceScreen from "../ChoiceScreen"
+import PickCre8orType from "../PickCre8orType"
+import { ChoiceOptions } from "../../lib/enums"
+import FindCre8orType from "../FindCre8orType"
 
 const AllowList = () => {
-  const { handleQuizSubmission, showQuiz, signedUp, showSkeleton } = useCre8orsProvider()
-  const [choice, setChoice] = React.useState(0)
+  const { showQuiz, signedUp, showSkeleton, creatorType } = useCre8orsProvider()
+  const [choice, setChoice] = React.useState(ChoiceOptions.Undecided)
   const onClickHandler = (value: number) => {
     setChoice(value)
   }
   const { width, height } = useWindowSize()
   return (
-    <ChoiceScreen onClickHandler={onClickHandler} />
-    // <div className="flex items-center justify-center h-screen">
-    //   {showQuiz && (
-    //     <Widget
-    //       id={process.env.NEXT_PUBLIC_TYPEFORM_ID}
-    //       style={{ width: "100%", height: "100%" }}
-    //       onSubmit={handleQuizSubmission}
-    //     />
-    //   )}
-    //   {!showQuiz && !showSkeleton && <AllowListForm />}
-    //   {showSkeleton && <SkeletonCard />}
-    //   {signedUp && <Confetti width={width} height={height} />}
-    // </div>
+    <div className="flex flex-row items-center justify-center w-full h-screen">
+      {choice === ChoiceOptions.Undecided && !creatorType && (
+        <ChoiceScreen onClickHandler={onClickHandler} />
+      )}
+      {choice === ChoiceOptions.PickYourCre8orType && !creatorType && <PickCre8orType />}
+      {choice === ChoiceOptions.FindYourCre8orType && !creatorType && <FindCre8orType />}
+      {((!showQuiz && !showSkeleton) || creatorType.length > 0) && <AllowListForm />}
+      {showSkeleton && <SkeletonCard />}
+      {signedUp && <Confetti width={width} height={height} />}
+    </div>
   )
 }
 
