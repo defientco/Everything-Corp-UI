2313f145f54746cb10c632953477df2adb3a7449
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-07 10:02:39 -0300

    I use enum mapping.

--- components/VideoPage/VideoPage.tsx
+++ components/VideoPage/VideoPage.tsx
@@ -1,8 +1,8 @@
 import { useRef, useState } from "react"
-import Image from "next/image"
 import useWindowSize from "../../lib/useWindowSize"
+import BackToRoadmapButton from "../BackToRoadmapButton/BackToRoadmapButton"
 
-const VideoPage = ({ goBack }: any) => {
+const VideoPage = () => {
   const [playing, setPlaying] = useState(false)
   const [soundOn, setSoundOn] = useState(true)
   const { width, height } = useWindowSize()
@@ -14,22 +14,7 @@ const VideoPage = ({ goBack }: any) => {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <button
-        onClick={goBack}
-        type="button"
-        className="absolute z-[10] left-10 top-10 p-5 border border-[#be0e11] hover:bg-white hover:text-[#be0e11] rounded-l-3xl rounded-r-lg flex align-middle gap-3 sm:text-xl text-xs"
-      >
-        <div className="border-r border-[#be0e11] h-full pr-3 flex align-middle">
-          <Image
-            src="/logo.png"
-            height="20px"
-            width="30px"
-            alt="logo"
-            className="border-r border-gray-400 h-full"
-          />
-        </div>
-        &larr; Back to Roadmap
-      </button>
+      <BackToRoadmapButton />
       {playing ? (
         <button
           onClick={() => setSoundOn(!soundOn)}
