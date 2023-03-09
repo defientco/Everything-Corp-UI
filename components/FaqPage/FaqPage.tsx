4420a5509272a96aeb8c02a7cc82e6f24b5c534d
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-09 07:46:45 -0300

    I init FAQ page.

---
+++ components/FaqPage/FaqPage.tsx
@@ -0,0 +1,10 @@
+import BackToRoadmapButton from "../BackToRoadmapButton"
+
+const FaqPage = () => (
+  <div className="h-screen flex items-center justify-center">
+    <BackToRoadmapButton textColor="#000000" />
+    <div className="w-[100vw] h-[100vh]">PUT FAQ HERE. MAP OVER JSON OF FAQ.</div>
+  </div>
+)
+
+export default FaqPage
