a227741d9601d5616b4dc3e435ddc43f30d2dd60
sameer rasa <crazzywizard@gmail.com>
2023-03-16 20:21:42 -0400

    added initial changes

---
+++ components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+const Header = () => (
+  <nav className="fixed top-0 z-50 flex flex-wrap items-center justify-between w-full p-6 bg-black border-b-4 font-aldrich">
+    <div className="items-center flex-shrink-0 hidden mr-6 text-white lg:flex">
+      <Image src="/evclogo.png" alt="EVC Logo" width={175} height={50} />
+    </div>
+    <div className="flex-col items-center w-full lg:md:flex-row lg:items-right lg:w-auto">
+      <a
+        href="/contact"
+        className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded font-aldrich font-weight-400 text-md hover:border-transparent lg:mt-0"
+      >
+        Contact
+      </a>
+      <a
+        href="/about"
+        className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded hover:border-transparent   lg:mt-0"
+      >
+        About
+      </a>
+      <a
+        href="/faq"
+        className="inline-block px-4 py-2 mt-4  text-white text-[32px] leading-[31px] font-[400] rounded text-center hover:border-transparent   lg:mt-0"
+      >
+        FAQ
+      </a>
+    </div>
+  </nav>
+)
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next"
 import { useState } from "react"
+import Image from "next/image"
 import SeoHead from "../components/SeoHead"
 import LaunchPage from "../components/LaunchPage"
 import RoadmapPage from "../components/RoadmapPage"
+import Header from "../components/Header"
 
 const Home: NextPage = () => {
   const [entered, setEntered] = useState(false)
 
   return (
-    <div className="bg-[#010e17] text-white">
-      <SeoHead title="CRE8ORS" description="CRE8ORS coming soon" image="" />
+    <div className="overflow-y-auto text-white bg-">
+      <SeoHead title="Evil Corp" description="Evil Corp" image="" />
+      <Header />
 
-      {entered ? <RoadmapPage /> : <LaunchPage onClick={setEntered} />}
+      <img
+        src="/LANDINGPAGE_BACKGROUND.png"
+        alt="EVC Logo"
+        className="w-full lg:h-[100vh] h-auto"
+      />
+      <div className="flex flex-col justify-around gap-4 m-4 lg:md:flex-row itmes-center">
+        <Image src="/PHOTO_RESTORATION.png" width={398} height={389} alt="restoration" />
+        <Image src="/PHOTO_UBIQUITY.png" width={398} height={389} alt="restoration" />
+        <Image src="/PHOTO_NEUTRALITY.png" width={398} height={389} alt="restoration" />
+      </div>
+      {/* {entered ? <RoadmapPage /> : <LaunchPage onClick={setEntered} />} */}
     </div>
   )
 }
