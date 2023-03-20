ffed0062dd97268cf2ea5495af5c141ba8f4dcaf
sameer rasa <crazzywizard@gmail.com>
2023-03-20 18:10:41 -0400

    refactored code

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -13,34 +13,32 @@ export const getApiKeys = async (collectionName) => {
   const result = Auth.findOne({ collectionName }).lean()
   return result
 }
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: NextFunction,
+  collectionName: string,
+) => {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    throw new NotFoundException("Authorization header not found")
+  }
+  const [, token] = authorization.split(" ")
+  const apiKeys = await getApiKeys(collectionName)
+  if (token !== apiKeys.apiKey) {
+    throw new UnauthorizedException("Invalid token")
+  }
+  next()
+}
+
 export const AllowListAuthGuard = createMiddlewareDecorator(
   async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
-    const { authorization } = req.headers
-
-    if (!authorization) {
-      throw new NotFoundException("Authorization header not found")
-    }
-    const [, token] = authorization.split(" ")
-    const apiKeys = await getApiKeys("AllowList")
-    if (token !== apiKeys.apiKey) {
-      throw new UnauthorizedException("Invalid token")
-    }
-    next()
+    handler(req, res, next, "AllowList")
   },
 )
 
 export const ParticipantsAuthGuard = createMiddlewareDecorator(
-  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
-    const { authorization } = req.headers
-
-    if (!authorization) {
-      throw new NotFoundException("Authorization header not found")
-    }
-    const [, token] = authorization.split(" ")
-    const apiKeys = await getApiKeys("Participants")
-    if (token !== apiKeys.apiKey) {
-      throw new UnauthorizedException("Invalid token")
-    }
-    next()
-  },
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) =>
+    handler(req, res, next, "Participants"),
 )
