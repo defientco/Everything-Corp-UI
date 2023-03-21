f0804e1b278b0b59220a2a35cdf132c00e943fd7
sameer rasa <crazzywizard@gmail.com>
2023-03-21 08:24:14 -0400

    making it mobile friendly

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Header = () => (
   <nav className="fixed top-0 z-50 flex items-center justify-between w-full p-6 bg-black border-b-4 font-aldrich">
-    <div className="relative items-center flex-shrink-0 mr-6 text-white cursor-auto w-36 lg:flex">
+    <div className="relative items-center flex-shrink-0 w-20 mr-6 text-white cursor-auto lg:md:w-36 lg:flex">
       <Link href="/">
         <Image
           src="/evclogo.png"
@@ -14,7 +14,7 @@ const Header = () => (
         />
       </Link>
     </div>
-    <div className="flex-col items-center w-full lg:md:flex-row text-lg lg:items-right lg:w-auto lg:md:text-[32px] lg:md:leading-[31px] font-[400] rounded font-aldrich font-weight-400 ">
+    <div className="items-center w-full flex-row text-md lg:items-right lg:w-auto lg:md:text-[32px] lg:md:leading-[31px] font-[400] rounded font-aldrich font-weight-400 ">
       <Link href="#contact">
         <div className="inline-block px-4 py-2 mt-4 text-white cursor-pointer text-md hover:border-transparent lg:mt-0">
           Contact
