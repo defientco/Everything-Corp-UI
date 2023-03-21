eb1646165b1b065e6091d78aa18766fa9876b6a2
sameer rasa <crazzywizard@gmail.com>
2023-03-20 21:40:54 -0400

    added evil and fixers pages

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => (
-  <nav className="fixed top-0 z-50 flex flex-wrap items-center justify-between w-full p-6 bg-black border-b-4 font-aldrich">
-    <div className="items-center flex-shrink-0 hidden mr-6 text-white cursor-auto lg:flex">
+  <nav className="fixed top-0 z-50 flex items-center justify-between w-full p-6 bg-black border-b-4 font-aldrich">
+    <div className="relative items-center flex-shrink-0 mr-6 text-white cursor-auto w-36 lg:flex">
       <Link href="/">
         <Image
           src="/evclogo.png"
@@ -14,25 +14,25 @@ const Header = () => (
         />
       </Link>
     </div>
-    <div className="flex-col items-center w-full lg:md:flex-row lg:items-right lg:w-auto">
+    <div className="flex-col items-center w-full lg:md:flex-row text-lg lg:items-right lg:w-auto lg:md:text-[32px] lg:md:leading-[31px] font-[400] rounded font-aldrich font-weight-400 ">
       <Link href="#contact">
-        <div className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded font-aldrich font-weight-400 text-md hover:border-transparent lg:mt-0 cursor-pointer">
+        <div className="inline-block px-4 py-2 mt-4 text-white cursor-pointer text-md hover:border-transparent lg:mt-0">
           Contact
         </div>
       </Link>
       <Link
         href="#about"
-        className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded hover:border-transparent   lg:mt-0"
+        className="inline-block px-4 py-2 mt-4 text-white hover:border-transparent lg:mt-0"
       >
-        <div className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded font-aldrich font-weight-400 text-md hover:border-transparent lg:mt-0 cursor-pointer">
+        <div className="inline-block px-4 py-2 mt-4 text-white cursor-pointer text-md hover:border-transparent lg:mt-0">
           About
         </div>
       </Link>
       <Link
         href="/faq"
-        className="inline-block px-4 py-2 mt-4  text-white text-[32px] leading-[31px] font-[400] rounded text-center hover:border-transparent   lg:mt-0"
+        className="inline-block px-4 py-2 mt-4 text-center text-white hover:border-transparent lg:mt-0"
       >
-        <div className="inline-block px-4 py-2 mt-4 text-white text-[32px] leading-[31px] font-[400] rounded font-aldrich font-weight-400 text-md hover:border-transparent lg:mt-0 cursor-pointer">
+        <div className="inline-block px-4 py-2 mt-4 text-white cursor-pointer hover:border-transparent lg:mt-0">
           FAQ
         </div>
       </Link>
