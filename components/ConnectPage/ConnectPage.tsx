f8eb1cf7e7ad529131ead63696bc0cb6b65e1f25
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-23 23:55:32 -0300

    I add Twitter Connect button

--- components/ConnectPage/ConnectPage.tsx
+++ components/ConnectPage/ConnectPage.tsx
@@ -1,16 +1,44 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import { useAccount } from "wagmi"
+import { signIn, useSession } from "next-auth/react"
 import { Button } from "../../shared/Button"
 import NavBar from "../NavBar"
 
 function ConnectPage() {
   const { address } = useAccount()
+  const { data: session, status } = useSession()
 
   return (
     <div className="mt-3 flex flex-col">
       {address && <NavBar />}
       <div className="flex flex-col items-center justify-around text-white pt-10 h-[75vh]">
-        {address ? <Button>Connect Twitter</Button> : <ConnectButton />}
+        {address ? (
+          // <Button type="button" onClick={handleClick}>
+          //   Connect Twitter
+          // </Button>
+          <a
+            href="/api/auth/signin"
+            className=""
+            onClick={(e) => {
+              e.preventDefault()
+              signIn("twitter")
+            }}
+          >
+            Sign in
+          </a>
+        ) : (
+          <ConnectButton />
+        )}
+        {session?.user && (
+          <>
+            <span style={{ backgroundImage: `url(${session.user.image})` }} />
+            <span>
+              <small>Signed in as</small>
+              <br />
+              <strong>{session.user.email || session.user.name}</strong>
+            </span>
+          </>
+        )}
       </div>
     </div>
   )
