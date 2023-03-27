ac0e6a9feb48ea8102afc7646a05073ec23f642e
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-27 20:55:32 -0300

    I add Ethereum RPC provider for ENS name support.

--- lib/getDefaultProvider.tsx
+++ lib/getDefaultProvider.tsx
@@ -1,18 +1,25 @@
 import { ethers } from "ethers"
 
+const ETH_RPC = "https://rpc.flashbots.net"
 const GOERLI_RPC = "https://ethereum-goerli-rpc.allthatnode.com"
+const POLYGON_RPC = "https://polygon.llamarpc.com"
+const MUMBAI_RPC = "https://rpc.ankr.com/polygon_mumbai"
 
 const getDefaultProvider = (chainId: number) => {
+  if (chainId === 1) {
+    return ethers.getDefaultProvider(ETH_RPC)
+  }
+
   if (chainId === 5) {
     return ethers.getDefaultProvider(GOERLI_RPC)
   }
 
-  if (chainId === 80001) {
-    return ethers.getDefaultProvider("https://rpc.ankr.com/polygon_mumbai")
+  if (chainId === 137) {
+    return ethers.getDefaultProvider(POLYGON_RPC)
   }
 
-  if (chainId === 137) {
-    return ethers.getDefaultProvider("https://polygon.llamarpc.com")
+  if (chainId === 80001) {
+    return ethers.getDefaultProvider(MUMBAI_RPC)
   }
 
   return ethers.getDefaultProvider(GOERLI_RPC)
