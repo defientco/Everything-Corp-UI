7e7736d4fcc2615c1ca0add036d7561d13380d76
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-27 20:55:51 -0300

    I update CollectorPage with ENS name & Avatar.

--- components/CollectorPage/CollectorPage.tsx
+++ components/CollectorPage/CollectorPage.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { BigNumber } from "ethers"
 import { useRouter } from "next/router"
+import { useEnsAvatar, useEnsName } from "wagmi"
 import ImageCard from "./ImageCard"
 import balanceOfParticipationRewards from "../../lib/balanceOfParticipationRewards"
 import truncateEthAddress from "../../lib/truncateEthAddress"
+import Image from "next/image"
 
 const NUMBER_OF_TOKENS = "0"
 
@@ -11,6 +13,14 @@ function CollectorPage() {
   const router = useRouter()
   const { collectorId } = router.query
   const [balance, setBalance] = useState(NUMBER_OF_TOKENS)
+  const { data: ensName } = useEnsName({
+    address: (collectorId as any) || "0x0",
+    chainId: 1,
+  })
+  const { data: ensAvatar } = useEnsAvatar({
+    address: (collectorId as any) || "0x0",
+    chainId: 1,
+  })
 
   useEffect(() => {
     const init = async () => {
@@ -20,12 +30,28 @@ function CollectorPage() {
     }
 
     init()
-  })
+  }, [collectorId])
+  console.log("collectorId", collectorId)
+  console.log("ensName", ensName)
+  console.log("ensAvatar", ensAvatar)
 
   return (
     <div className="mt-3 flex flex-col">
       <div className="flex flex-col items-center justify-around text-4xl text-white pt-10 h-[75vh]">
-        <div>CRE8OR Profile - {truncateEthAddress(collectorId as string)}</div>
+        <div className="flex items-center gap-11">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={ensAvatar || "/logo-cre8ors.png"}
+            alt="pfp"
+            width={100}
+            height={100}
+            className="rounded"
+          />
+          <div>
+            <div>CRE8OR Profile</div>
+            <div>{ensName || truncateEthAddress(collectorId as string)}</div>
+          </div>
+        </div>
         {BigNumber.from(balance).gt(0) && (
           <div className="flex flex-col items-center gap-5">
             <ImageCard
