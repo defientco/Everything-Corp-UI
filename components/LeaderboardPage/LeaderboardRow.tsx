2e2c3ffe467b0418f55f8d301ad3bc3b65bf8c85
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-27 22:32:26 -0300

    I link Leaderboard to profile with ens images

--- components/LeaderboardPage/LeaderboardRow.tsx
+++ components/LeaderboardPage/LeaderboardRow.tsx
@@ -1,25 +1,21 @@
+import { getAddress } from "ethers/lib/utils"
 import Link from "next/link"
-import { toast } from "react-toastify"
 import { useEnsName } from "wagmi"
 import truncateEthAddress from "../../lib/truncateEthAddress"
+import PFP from "../PFP/PFP"
 
 const LeaderboardRow = ({ address, numberOwned, rank, twitterHandle }) => {
   const { data: ensName } = useEnsName({
     address,
     chainId: 1,
   })
-  console.log("ensName", ensName)
-
-  const handleCopyClick = async () => {
-    await navigator.clipboard.writeText(address)
-    toast.success("copied to clipboard!")
-  }
 
   return (
     <tr key={address} className="bg-gray-100  hover:bg-blue-300">
       <td className="px-4 py-2 border-b">#{rank}</td>
       <td className="px-4 py-2 border-b">{numberOwned}</td>
-      <td className="px-4 py-2 border-b">
+      <td className="px-4 py-2 border-b flex items-center gap-3">
+        <PFP address={getAddress(address)} height={25} width={25} />
         <Link href={`/collector/${address}`} type="button">
           {ensName || truncateEthAddress(address)}
         </Link>
