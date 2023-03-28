fc25518d2ecb8913e919123604d4ef235b34839c
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-28 15:03:19 -0300

    I add lib to convert EVM epochSeconds to human readable date.

---
+++ lib/epochToReadableDate.ts
@@ -0,0 +1,28 @@
+const SCALAR = 1000
+
+const epochToReadableDate = (epochSeconds: number) => {
+    console.log("epochToReadableDate", epochSeconds)
+  const date = new Date(epochSeconds * SCALAR)
+
+  const day = date.getDate()
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+  const month = monthNames[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${day} ${month}, ${year}`
+}
+
+export default epochToReadableDate
