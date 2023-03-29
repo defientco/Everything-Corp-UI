7847a00260601069f3f9f0256d358994bf27ce8d
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-29 17:40:54 -0300

    I switch from chaining ifs to a switch statement.

--- lib/alchemy/getAlchemyBaseUrl.ts
+++ lib/alchemy/getAlchemyBaseUrl.ts
@@ -1,13 +1,15 @@
 const ETH = "https://eth-mainnet.g.alchemy.com/"
 const GOERLI = "https://eth-goerli.g.alchemy.com/"
+
 const getAlchemyBaseUrl = (chainId: number) => {
-  if (chainId === 1) {
-    return ETH
+  switch (chainId) {
+    case 1:
+      return ETH
+    case 5:
+      return GOERLI
+    default:
+      return ETH
   }
-  if (chainId === 5) {
-    return GOERLI
-  }
-  return ETH
 }
 
 export default getAlchemyBaseUrl
