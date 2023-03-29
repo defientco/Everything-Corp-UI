481c1dada4516c85a067f21aaf2fca41301b870b
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-29 15:24:28 -0300

    I DRY code with collectorIdAsString variable.

--- components/CollectorPage/CollectorPage.tsx
+++ components/CollectorPage/CollectorPage.tsx
@@ -16,42 +16,43 @@ const NUMBER_OF_TOKENS = "0"
 function CollectorPage() {
   const router = useRouter()
   const { collectorId } = router.query
+  const collectorIdAsString = collectorId.toString()
   const [balance, setBalance] = useState(NUMBER_OF_TOKENS)
   const [anniversary, setAnniversary] = useState(null as string)
   const [twitter, setTwitter] = useState(null as string)
   const [ens, setEns] = useState({} as any)
 
   const handleCopyClick = async () => {
-    await navigator.clipboard.writeText(collectorId as string)
+    await navigator.clipboard.writeText(collectorIdAsString)
     toast.success("copied to clipboard!")
   }
 
   useEffect(() => {
     const init = async () => {
-      const response = await balanceOfParticipationRewards(collectorId as string)
+      const response = await balanceOfParticipationRewards(collectorIdAsString)
       if (response.error) return
       setBalance(response.toString())
-      const epoch = await getAnniversary(collectorId as string)
+      const epoch = await getAnniversary(collectorIdAsString)
       const readable = epochToReadableDate(epoch)
       setAnniversary(readable)
-      const handle = await getTwitterHandle(collectorId as string)
+      const handle = await getTwitterHandle(collectorIdAsString)
       setTwitter(handle)
-      const ensRecord = await getEns(collectorId as string)
+      const ensRecord = await getEns(collectorIdAsString)
       setEns(ensRecord)
     }
 
     init()
-  }, [collectorId])
+  }, [collectorIdAsString])
 
   return (
     <div className="mt-3 flex flex-col">
       <div className="flex flex-col items-center justify-around text-4xl text-white pt-10 h-[75vh]">
         <div className="flex items-center gap-11">
-          <PFP address={(collectorId as string) || "0x0"} />
+          <PFP address={collectorIdAsString || "0x0"} />
           <div>
             <div>CRE8OR Profile</div>
             <button onClick={handleCopyClick} type="button">
-              {ens?.title || truncateEthAddress(collectorId as string)}
+              {ens?.title || truncateEthAddress(collectorIdAsString)}
             </button>
             {anniversary && <div className="text-sm">Joined {anniversary}</div>}
             {twitter && <div className="text-sm">Twitter: {twitter}</div>}
