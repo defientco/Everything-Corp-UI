0feb24d65bce3f980336419f6a01662c84a00901
sameer rasa <crazzywizard@gmail.com>
2023-04-11 19:52:01 -0400

    added interactive map

---
+++ components/ImageModal/ImageModal.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { useEffect, useRef } from "react"
+
+const ImageModal = ({ imageUrl, showModal, setShowModal }) => {
+  const modalRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      console.log("event.target", event.target)
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShowModal(!showModal)
+      }
+    }
+
+    window.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [modalRef, setShowModal, showModal])
+  return (
+    showModal && (
+      <div className="fixed inset-0 z-10 mt-24 overflow-y-auto">
+        <div className="flex items-center justify-center w-full min-h-screen px-4">
+          <div className="relative w-full max-w-xl mx-auto overflow-hidden rounded-lg shadow-lg max-h-2xl bg-none">
+            <img src={imageUrl} alt="Modal" className="object-contain w-full" ref={modalRef} />
+          </div>
+        </div>
+      </div>
+    )
+  )
+}
+
+export default ImageModal
--- components/InteractiveStoryPage/InteractiveStoryPage.tsx
+++ components/InteractiveStoryPage/InteractiveStoryPage.tsx
@@ -1,26 +1,53 @@
 import { useState } from "react"
 import ImageMapper from "react-img-mapper"
-import Confetti from "react-confetti"
 import useWindowSize from "../../lib/useWindowSize"
-import BackToRoadmapButton from "../BackToRoadmapButton"
 import map from "../../lib/image-map.json"
+import ImageModal from "../ImageModal"
+import Header from "../Header"
 
+const PointsToUrl = {
+  1: "/color_red2.png",
+  2: "/codes2.png",
+  3: "/coins2.png",
+  4: "/color_and_sound2.png",
+  5: "/colors_of_jobs2.png",
+  6: "/decree2.png",
+  7: "/dna2.png",
+  8: "/EVIL2.png",
+  9: "/fixers_pic2.png",
+  10: "/journal1.png",
+  11: "/quiz2.png",
+  12: "/run2.png",
+  13: "/soot2.png",
+}
 const InteractiveStoryPage = () => {
-  const [puzzleCompleted, setPuzzleCompleted] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [imgUrl, setImgUrl] = useState("")
   const { width, height } = useWindowSize()
 
-  const handleClick = () => {
-    setPuzzleCompleted(true)
+  const handleClick = (area, index, e) => {
+    e.preventDefault()
+    setImgUrl(PointsToUrl[area.name])
+    setShowModal(true)
   }
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <BackToRoadmapButton textColor="#000000" />
-      <div className="w-[100vw] h-[100vh]">
-        <ImageMapper src="/logo-cre8ors.png" map={map} natural onClick={handleClick} />
+    <>
+      <Header />
+      <div className="flex justify-center mt-24">
+        <ImageMapper
+          src="/storytelling_ui.png"
+          map={map}
+          onClick={(area, index, e) => handleClick(area, index, e)}
+          responsive
+          height={height}
+          parentWidth={width}
+        />
+        {showModal && (
+          <ImageModal imageUrl={imgUrl} setShowModal={setShowModal} showModal={showModal} />
+        )}
       </div>
-      {puzzleCompleted && <Confetti width={width} height={height} />}
-    </div>
+    </>
   )
 }
 
