c5244ccf755fc4569e30fa42737cf666ac16d8f6
sameer rasa <crazzywizard@gmail.com>
2023-04-20 09:50:27 -0400

    fixing deployment issues

--- utils/getLogger.ts
+++ utils/getLogger.ts
@@ -4,6 +4,7 @@ const logger = new Logger()
 logger.settings.hideLogPositionForProduction = true
 logger.settings.stylePrettyLogs = true
 
-const getLogger = (name: string) => logger.getSubLogger({ name })
+const getLogger = (name: string, type: "json" | "pretty" | "hidden" = "json") =>
+  logger.getSubLogger({ name, type })
 
 export default getLogger
