de036e35c20da17ac5822aa2989eb4bf9c5a662a
sameer rasa <crazzywizard@gmail.com>
2023-05-02 22:19:15 -0400

    reverting prettying logs

--- utils/getLogger.ts
+++ utils/getLogger.ts
@@ -4,7 +4,7 @@ const logger = new Logger()
 logger.settings.hideLogPositionForProduction = true
 logger.settings.stylePrettyLogs = true
 
-const getLogger = (name: string, type: "json" | "pretty" | "hidden" = "pretty") =>
+const getLogger = (name: string, type: "json" | "pretty" | "hidden" = "json") =>
   logger.getSubLogger({ name, type })
 
 export default getLogger
