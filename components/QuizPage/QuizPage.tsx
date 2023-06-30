d7eb07a090d43a20b5f10d31b329aa7526cb2e5e
Sweets Sweetman <sweetmantech@gmail.com>
2023-06-30 17:23:35 -0300

    I use onSubmit callback.

--- components/QuizPage/QuizPage.tsx
+++ components/QuizPage/QuizPage.tsx
@@ -1,13 +1,23 @@
 import { Widget } from "@typeform/embed-react"
+import { useRouter } from "next/router"
 
-const QuizPage = () => (
-  <div className="w-screen h-screen">
-    <Widget
-      id={process.env.NEXT_PUBLIC_TYPEFORM_ID}
-      style={{ width: "100%", height: "100%" }}
-      className="my-form"
-    />
-  </div>
-)
+const QuizPage = () => {
+  const router = useRouter()
+
+  const onSubmit = ({ responseId }: any) => {
+    router.push(`https://cre8ors.com/quiz/success?responseId=${responseId}`)
+  }
+
+  return (
+    <div className="w-screen h-screen">
+      <Widget
+        id={process.env.NEXT_PUBLIC_TYPEFORM_ID}
+        style={{ width: "100%", height: "100%" }}
+        className="my-form"
+        onSubmit={onSubmit}
+      />
+    </div>
+  )
+}
 
 export default QuizPage
