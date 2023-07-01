c0480b282fcb740f05e2f66059e7626f4fb71c4a
Software Engineer <im.ziad.sakr@gmail.com>
2023-06-30 20:50:31 -0400

    remove onsubmit

--- components/QuizPage/QuizPage.tsx
+++ components/QuizPage/QuizPage.tsx
@@ -1,23 +1,13 @@
 import { Widget } from "@typeform/embed-react"
-import { useRouter } from "next/router"
 
-const QuizPage = () => {
-  const router = useRouter()
-
-  const onSubmit = ({ responseId }: any) => {
-    router.push(`https://cre8ors.com/status?responseId=${responseId}`)
-  }
-
-  return (
-    <div className="w-screen h-screen">
-      <Widget
-        id={process.env.NEXT_PUBLIC_TYPEFORM_ID}
-        style={{ width: "100%", height: "100%" }}
-        className="my-form"
-        onSubmit={onSubmit}
-      />
-    </div>
-  )
-}
+const QuizPage = () => (
+  <div className="w-screen h-screen">
+    <Widget
+      id={process.env.NEXT_PUBLIC_TYPEFORM_ID}
+      style={{ width: "100%", height: "100%" }}
+      className="my-form"
+    />
+  </div>
+)
 
 export default QuizPage
