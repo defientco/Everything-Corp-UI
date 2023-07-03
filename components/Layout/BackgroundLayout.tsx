866b2a239a599c77f5507116c1a8f6d3e56ee1ff
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-03 09:39:36 -0300

    I fix SEO info for Everything Corp.

--- components/Layout/BackgroundLayout.tsx
+++ components/Layout/BackgroundLayout.tsx
@@ -7,7 +7,7 @@ import Footer from "../Footer"
 function BackgroundLayout({ children }: ILayout) {
   return (
     <div className="h-screen text-black">
-      <SeoHead title="Evil Corp" description="Evil Corp" image="/evclogoIcon.png" />
+      <SeoHead title="Everything Corp" description="Everything Corp" image="/evclogoIcon.png" />
       <Header />
       <div className="relative z-[2] flex justify-center pt-[100px] md:pt-[180px] bg-[url('/FAQ/background.svg')] bg-cover min-h-[100vh] bg-[bottom_right]">
         <div className="max-w-[1280px]">
